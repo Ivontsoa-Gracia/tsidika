f9a9663813fdc1e44fc8e8bea39c816b72007628
Ivontsoa Gracia <graciaandriamihamina@gmail.com>
2025-03-19 14:26:53 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -12,3 +12,17 @@ export default defineConfig({
         }),
     ],
 });
+
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    outDir: 'public/build',
+    manifest: true,
+    rollupOptions: {
+      input: 'resources/js/app.js',
+    },
+  },
+});
